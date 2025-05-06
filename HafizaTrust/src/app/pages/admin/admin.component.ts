b70561be536fae541fbefb1e0b0601ea44f19080
ZainabAA <zainab.a.a.alarbash@gmail.com>
2025-05-06 10:01:45 +0300

    created users table in admin page

--- HafizaTrust/src/app/pages/admin/admin.component.ts
+++ HafizaTrust/src/app/pages/admin/admin.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {ViewChild, AfterViewInit, Component, effect, inject, signal} from '@angular/core';
+import {MatTableModule, MatTableDataSource} from '@angular/material/table';
+import { UserService } from '../../services/user/user.service';
+import { User } from '../../data/user';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  usersService = inject(UserService);
+  usersData = signal<User[]>([]);
+  dataSource = new MatTableDataSource<User>([]);
+
+  usersEffect = effect(() => {
+    this.usersService.getAllUsers().subscribe({
+      next: (res) => {
+        this.usersData.set(res);
+        this.dataSource = new MatTableDataSource<User>(this.usersData());
+        this.dataSource.paginator = this.paginator;
+      }
+    })
+  })
+  
+  isImageError = false;
+  displayedColumns: string[] = ['id', 'username', 'balance', 'image'];
 
 }
