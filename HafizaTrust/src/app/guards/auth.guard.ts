19e034978eafe84d5953b20c92afc9ce5f0f72bd
ZainabAA <zainab.a.a.alarbash@gmail.com>
2025-05-07 09:51:30 +0300

    added interceptor + removed notes + updated services to use inteceptor

--- HafizaTrust/src/app/guards/auth.guard.ts
+++ HafizaTrust/src/app/guards/auth.guard.ts
@@ -1,26 +1,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-<<<<<<< HEAD
-export function getToken(name: string): string | null {
+import { getToken } from '../interceptors/auth.interceptor';
 
-=======
-
-export function getToken(name: string): string | null {
->>>>>>> f1782f1e08877ee26aa25a4c754d2ea6306e4c76
-  const cookies = document.cookie.split('; ');
-  for (const cookie of cookies) {
-    const [key, value] = cookie.split('=');
-    if (key === name) {
-      return decodeURIComponent(value);
-    }
-  }
-  return null;
-}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> f1782f1e08877ee26aa25a4c754d2ea6306e4c76
 export const authGuard: CanActivateFn = (route, state) => {
 
   let token = getToken('token');
