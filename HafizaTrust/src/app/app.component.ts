bb40cc5b5e0516fec1bad68ae81cd1d306cb7ae0
Fatemah AlAttar <falatar@pifss.gov.kw>
2025-05-10 01:30:39 +0300

    side nav tree

--- HafizaTrust/src/app/app.component.ts
+++ HafizaTrust/src/app/app.component.ts
@@ -11,11 +11,31 @@ import { User } from './interfaces/user';
 import { UserService } from './services/user/user.service';
 import { AuthService } from './services/auth/auth.service';
 import { MatButtonModule } from '@angular/material/button';
+import {MatTreeFlatDataSource, MatTreeFlattener, MatTreeModule} from '@angular/material/tree';
+import {FlatTreeControl} from '@angular/cdk/tree';
+
+
+interface MenuNode {
+  name: string;
+  children?: MenuNode[];
+  route?: string;
+}
+
+const TREE_DATA: MenuNode[] = [
+  {
+    name: 'Services',
+    children: [ {name: 'Transactions', route: '/user/transactions'}, {name: 'Beneficiaries', route:'user/beneficiaries'}],
+  }]
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatFormFieldModule, MatInputModule, MatIconModule, HeaderComponent, LoaderComponent, MatSidenavModule, RouterLink, MatButtonModule],
+  imports: [RouterOutlet, MatFormFieldModule, MatInputModule, MatIconModule, HeaderComponent, LoaderComponent, MatSidenavModule, RouterLink, MatButtonModule,MatTreeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -49,4 +69,35 @@ user: User | null = null;
   {
     this.sidenavTrigger.set(val)
   }
+
+  /*
+  Tree code
+   */
+  private _transformer = (node: MenuNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable,
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children,
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 }
